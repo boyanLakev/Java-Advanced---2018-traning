cd899b3e2b26d214a401dc473bcfa1041047b2df
boyanLakev <blakev@abv.bg>
2019-09-22 14:40:25 +0300

    :)

--- Sets And Maps - Exercises/src/LegendaryFarming.java
+++ Sets And Maps - Exercises/src/LegendaryFarming.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class LegendaryFarming {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+
+
+
 
 
     }
